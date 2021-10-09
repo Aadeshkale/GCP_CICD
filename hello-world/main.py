d143a4082bffd670aea3a8757e597444b7f4bf6e
Aadesh Kale <40000735+Aadeshkale@users.noreply.github.com>
2021-10-09 11:36:00 +0530

    Hello World Application added

---
+++ hello-world/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Simple Hello web app for demo of GCP!'
+
+@app.route('/display')
+def dis():
+    return 'Display info!'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
